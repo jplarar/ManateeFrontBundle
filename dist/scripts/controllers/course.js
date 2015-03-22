e64d6fc66156b0bbb3f3c2e3f984d6fe16d8f6e8
luisvcastilla <luis@sititec.com>
2015-03-22 13:35:43 -0600

    asdf

--- dist/scripts/controllers/course.js
+++ dist/scripts/controllers/course.js
@@ -10,7 +10,26 @@
 angular.module('skillsmeisterApp')
   .controller('CourseCtrl', function ($scope, $rootScope, $log, SweetAlert, $routeParams, appSharedService, $timeout) {
   	$log.info($routeParams.id);
-  	// $scope.getListing = function() {
+  	
+  	$scope.buy = function () {
+  		console.log($scope.course.listingId);
+  		var data = {listingId: $scope.course.listingId};
+  		console.log(data)
+  		var promise = appSharedService.newPointLog(data);
+  		promise.then(function (res) {
+  			var res = res.response;
+  			if(res.error != 0 ) {
+  				SweetAlert.swal('Lo sentimos', res.description, 'warning');
+  				return;
+  			}
+  			SweetAlert.swal('Muy bien', 'Acabas de adquirir el curso de ' + $scope.course.name, 'success');
+  			$scope.bought = true;
+  			res.data[0].categoryImage = _.result(_.findWhere($rootScope.categories, { 'categoryId': res.data[0].category }), 'imageUrl');
+    		$log.info(res.data[0])
+			$scope.course = res.data[0];
+  		})
+  	};
+
 	var promise = appSharedService.getListing($routeParams.id);
   	promise.then(function(res) {
   		var res = res.response;
@@ -21,14 +40,17 @@ angular.module('skillsmeisterApp')
 		$timeout(function() {
     		res.data[0].categoryImage = _.result(_.findWhere($rootScope.categories, { 'categoryId': res.data[0].category }), 'imageUrl');
     		$log.info(res.data[0])
-		$scope.course = res.data[0];
+			$scope.course = res.data[0];
+			if($scope.course.phoneNumber != 'undefined') {
+				$scope.bought = true;
+			}
     	}, 300);
   	});
 	$scope.reviews = [
 		{comment: 'La verdad me encanto el curso, fue lo mejor poderlo llevar.', rating: 5, user: 'Luis Villarreal'},
 		{comment: 'Me pareció muy bueno pero pense que era un poco más especializado.', rating: 4, user: 'Juan Paulo Lara'}
 	]
-	$scope.rate = 2;
+	$scope.rate = 0;
 	$scope.max = 5;
 	$scope.isReadonly = false;
 
