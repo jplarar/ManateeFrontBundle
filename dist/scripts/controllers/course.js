d3cfa8f1144f5fe0671643af8e7db86b06d7c9ba
luisvcastilla <luis@sititec.com>
2015-03-22 13:59:28 -0600

    asdf

--- dist/scripts/controllers/course.js
+++ dist/scripts/controllers/course.js
@@ -23,32 +23,32 @@ angular.module('skillsmeisterApp')
   				return;
   			}
   			SweetAlert.swal('Muy bien', 'Acabas de adquirir el curso de ' + $scope.course.name, 'success');
-  			$scope.bought = true;
-  			res.data[0].categoryImage = _.result(_.findWhere($rootScope.categories, { 'categoryId': res.data[0].category }), 'imageUrl');
-    		$log.info(res.data[0])
-			$scope.course = res.data[0];
+  			$scope.getListing();
   		})
   	};
-
-	var promise = appSharedService.getListing($routeParams.id);
-  	promise.then(function(res) {
-  		var res = res.response;
-		if(res.error!==0) {
-			SweetAlert.swal('Hubo un error', res.description, 'error');
-			return;
-		}
-		$timeout(function() {
+	$scope.getListing = function() {
+		var promise = appSharedService.getListing($routeParams.id);
+	  	promise.then(function(res) {
+	  		var res = res.response;
+			if(res.error!==0) {
+				SweetAlert.swal('Hubo un error', res.description, 'error');
+				return;
+			}
+			// $timeout(function() {
     		res.data[0].categoryImage = _.result(_.findWhere($rootScope.categories, { 'categoryId': res.data[0].category }), 'imageUrl');
-    		$log.info(res.data[0])
+    		// $log.info(res.data[0])
 			$scope.course = res.data[0];
-			$log.info(res.data[0].phoneNumber);
-			if(res.data[0].phoneNumber != 'undefined') {
+			// $log.info(res.data[0].phoneNumber);
+			if(res.data[0].phoneNumber == 'undefined') {
 				$scope.bought = false;
 			} else {
 				$scope.bought = true;
 			}
-    	}, 300);
-  	});
+	    	// }, 300);
+	  	});
+	};
+	$scope.getListing();
+
 	$scope.reviews = [
 		{comment: 'La verdad me encanto el curso, fue lo mejor poderlo llevar.', rating: 5, user: 'Luis Villarreal'},
 		{comment: 'Me pareció muy bueno pero pense que era un poco más especializado.', rating: 4, user: 'Juan Paulo Lara'}
